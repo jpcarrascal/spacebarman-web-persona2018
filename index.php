884c36038cee89863c6748ee0001ec43ca5a45ca
JP Carrascal <juanpcarrascal@gmail.com>
2020-08-16 10:56:24 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-15242862-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-15242862-2');
+</script>
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width,user-scalable =no" />
+<meta name="description" content="Spacebarman is an electronic rock band from Barcelona.">
+<meta name="apple-mobile-web-app-capable" content="yes" />
+
+<title>Spacebarman</title>
+
+
+
+<!---
+<script src="jquery-3.3.1.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+
+
+});
+
+</script>
+--->
+
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway" />
+<link href="reset.css" type="text/css" rel="stylesheet"/>
+<link href="style.css" type="text/css" rel="stylesheet"/>
+</head>
+
+
+
+<body>
+
+<div id="top_bar" class="top_bar">
+
+    <div class="bar_item media" text="music"  id="music">
+    <a href="https://open.spotify.com/artist/1o8zzq05yXKrsdQUrMzZdc?si=qEi8unstQ5qMCWFrdX2LAA" target=_blank>
+        music
+    </a>
+    </div>
+    
+    <div class="bar_item media" text="videos" id="videos">
+    <a href="https://www.youtube.com/user/spacebarmanvideo" target=_blank>
+        video
+    </a>
+    </div>
+
+    <div class="bar_item media" text="photos" id="photos">
+    <a href="http://flickr.com/photos/spacebarman" target=_blank>
+        photos
+    </a>
+    </div>
+    
+    <!---
+    <div class="bar_item" id="blog">
+    <a href="http://spacebarman.com/blog" target=_blank>
+        blog
+    </a>
+    </div>
+
+    <div class="bar_item" id="connect">        
+        follow us
+        <a href="https://www.instagram.com/spacebarman/" target=_blank><div class="bar_subitem">instagram</div></a>
+        <a href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fspacebarman.com%2F&region=follow_link&screen_name=spacebarman&tw_p=followbutton&variant=2.0" target=_blank><div class="bar_subitem">twitter</div></a>
+        <a href="https://facebook.com/spacebarman" target=_blank><div class="bar_subitem">facebook</div></a>
+        <a href="http://eepurl.com/dnDDUH" target=_blank><div class="bar_subitem" id="subscribe">subscribe</div></a>
+        <a href="mailto:info@spacebarman.com" target=_blank><div class="bar_subitem">email us</div></a>
+    </div>
+    --->
+</div>
+
+
+
+<div id="bg" class="bg">
+    
+    <div id="profiles">    
+        <div class="profile">
+            <img class="profile-img" src="images/profile-red-left.png" />
+        </div>
+
+        <div class="profile right">
+            <img class="profile-img" src="images/profile-red-left.png" />
+        </div>
+    </div>
+    
+    <div id="middle">
+        <div class="middle-text">
+            new ep:
+        </div>
+        
+        <div class="title-img" >
+            <!-- <a href="https://open.spotify.com/album/7EHgHQbPN8vRqETINmIhH3?si=eZRtVLwiRgStnNeeVO_H6Q" target=_blank> -->
+            <img class="persona-clone" src="images/persona.png" />
+            <!-- </a> -->
+        </div>
+        
+        <div class="middle-text">
+        
+            <span class="red-text">listen now:</span>
+            <br /><br />
+
+            <iframe src="https://open.spotify.com/embed?uri=spotify:album:7EHgHQbPN8vRqETINmIhH3" width="300" height="307" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                
+            <!--
+            <a class="media" href="https://open.spotify.com/album/7EHgHQbPN8vRqETINmIhH3?si=eZRtVLwiRgStnNeeVO_H6Q" target=_blank>spotify</a>
+            <a class="media" href="https://soundcloud.com/spacebarman/sets/persona" target=_blank>soundcloud</a>
+            <br />
+            download: <a class="media" href="https://spacebarman.bandcamp.com/album/persona-ep" target=_blank>bandcamp</a>
+            <br />
+            -->
+        </div>
+<!---
+        <div class="middle-text">
+            new videoclip:
+        </div>
+        
+        <div class="title-img" >
+            <a href="https://www.youtube.com/watch?v=ha37oTBkqpQ" target=_blank><img class="persona-clone" src="images/clone.png" /></a>
+        </div>
+
+        <div class="middle-text">
+            watch it now on <a class="media" href="https://www.youtube.com/watch?v=ha37oTBkqpQ" target=_blank>youtube</a>
+        </div>
+--->
+
+        <div class="middle-text follow-icon-container">
+            <a href="https://www.instagram.com/spacebarman/" target=_blank>
+                <div class="follow-icon follow-ins" src="images/instagram-gold.png"></div>
+            </a>
+            <a href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fspacebarman.com%2F&region=follow_link&screen_name=spacebarman&tw_p=followbutton&variant=2.0" target=_blank>
+                <div class="follow-icon follow-twi" src="images/twitter-gold.png"></div>
+            </a>
+            <a href="https://facebook.com/spacebarman" target=_blank>
+                <div class="follow-icon follow-fbk" style="width:28px;height:28px;" src="images/facebook-gold.png"></div>
+            </a>
+            <a href="https://spacebarman.us12.list-manage.com/subscribe?u=08fa2b83e7de09bfd19384dee&id=08f0d07b1e" target=_blank>
+                <div class="follow-icon follow-sub" src="images/mail-gold.png"></div>
+            </a>
+            
+        </div>
+
+    </div>
+
+    <div id="credits"><a id="jp" href="http://www.jpcarrascal.com" target=_blank>designed and coded by jp</a></div>
+</div>
+
+
+</body>
+</html>
